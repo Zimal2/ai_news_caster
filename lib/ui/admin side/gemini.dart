187355b31c339c934dfc58ce61a1de89eba75864
Rameenfa <150556581+Rameenfa@users.noreply.github.com>
2024-03-18 23:01:43 +0500

    gemini loading added

--- lib/ui/admin side/gemini.dart
+++ lib/ui/admin side/gemini.dart
@@ -3,7 +3,7 @@ import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:intl/intl.dart';
 
 class Gemini extends StatefulWidget {
-  const Gemini({super.key});
+  const Gemini({Key? key});
 
   @override
   State<Gemini> createState() => _GeminiState();
@@ -14,51 +14,54 @@ class _GeminiState extends State<Gemini> {
   TextEditingController _geminiTextController = TextEditingController();
   final model = GenerativeModel(model: 'gemini-pro', apiKey: apiKey);
   final List<Message> _message = [];
+  bool _isFetchingResponse = false; // Added boolean variable
 
-//method to send message
+  // Method to send message
   Future<void> sendMessage() async {
     final message = _geminiTextController.text;
     _geminiTextController.clear();
     setState(() {
-      _message
-          .add(Message(date: DateTime.now(), isUser: true, message: message));
+      _message.add(Message(date: DateTime.now(), isUser: true, message: message));
     });
     final content = [Content.text(message)];
+    
+    setState(() {
+      _isFetchingResponse = true; // Set to true when fetching response
+    });
+    
     final response = await model.generateContent(content);
     setState(() {
-      _message.add(Message(
-          date: DateTime.now(), isUser: false, message: response.text ?? ""));
+      _isFetchingResponse = false; // Set to false after fetching response
+      _message.add(Message(date: DateTime.now(), isUser: false, message: response.text ?? ""));
     });
 
     // Pass the response to the UploadNews screen
-  Navigator.pop(context, response.text);
+    Navigator.pop(context, response.text);
   }
 
-  bool _isChattingAllowed = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: Color.fromARGB(255, 104, 103, 103),
       appBar: AppBar(title: Text("Gemini")),
-
       body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
         child: Column(
           children: [
             Center(
               child: Container(
-                  height: 50,
-                  width: 250,
-                  decoration: BoxDecoration(
-                      color: Colors.green,
-                      border: Border.all(color: Colors.black),
-                      borderRadius: BorderRadius.circular(35)),
-                  child: Center(
-                    child: Text(
-                      "Note: You can only enter one query",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  )),
+                height: 50,
+                width: 250,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  border: Border.all(color: Colors.black),
+                  borderRadius: BorderRadius.circular(35),
+                ),
+                child: Center(
+                  child: Text(
+                    "Note: You can only enter one query",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
             ),
             Container(
               height: MediaQuery.of(context).size.height * 0.8,
@@ -68,9 +71,11 @@ class _GeminiState extends State<Gemini> {
                 itemBuilder: (context, index) {
                   final message = _message[index];
                   return Messages(
-                      date: DateFormat('HH:mm').format(message.date),
-                      isUser: message.isUser,
-                      message: message.message);
+                    date: DateFormat('HH:mm').format(message.date),
+                    isUser: message.isUser,
+                    message: message.message,
+                    isFetching: _isFetchingResponse,
+                  );
                 },
               ),
             ),
@@ -82,65 +87,26 @@ class _GeminiState extends State<Gemini> {
         children: [
           Expanded(
             child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: _isChattingAllowed
-                    ? TextFormField(
-                        controller: _geminiTextController,
-                        decoration: InputDecoration(
-                            fillColor: Colors.black45,
-                            filled: true,
-                            hintText: "Enter your message",
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(50)),
-                            // focusedBorder: border,
-                            // disabledBorder: border,
-                            // enabledBorder: border,
-                            suffixIcon: IconButton(
-                                onPressed: () async {
-                                  _isChattingAllowed = false;
-                                  sendMessage();
-                                  // await bloc
-                                  //     .getAiResponse(controller.text, context)
-                                  //     .then((value) {
-                                  //   controller.text = '';
-                                  // });
-                                },
-                                icon: const Icon(Icons.send))),
-                      )
-                    : Container(
-                        height: 50,
-                        width: 200,
-                        decoration: BoxDecoration(
-                            color: Colors.black45,
-                            borderRadius: BorderRadius.circular(35)),
-                        child: Center(
-                          child: Text("Cant enter more queries"),
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                      controller: _geminiTextController,
+                      decoration: InputDecoration(
+                        fillColor: Colors.black45,
+                        filled: true,
+                        hintText: "Enter your message",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50),
                         ),
-                      )),
+                        suffixIcon: IconButton(
+                          onPressed: () async {
+                            sendMessage();
+                          },
+                          icon: Icon(Icons.send),
+                        ),
+                      ),
+                    ),
+            ),
           ),
-          // InkWell(
-          //     onTapDown: (value) async {
-          //       var available = await speech.initialize();
-          //       if (available) {
-          //         speech.listen(
-          //           onResult: (result) {
-          //             setState(() {
-          //               controller.text = result.recognizedWords;
-          //               debugPrint('stda ${result.recognizedWords}');
-          //             });
-          //           },
-          //         );
-          //       }
-          //     },
-          //     onTapUp: (details) {
-          //       setState(() {
-          //         speech.stop();
-          //       });
-          //     },
-          //     child: const Icon(
-          //       Icons.mic,
-          //       size: 30,
-          //     ))
         ],
       ),
     );
@@ -151,31 +117,33 @@ class Messages extends StatelessWidget {
   final bool isUser;
   final String message;
   final String date;
+  final bool isFetching;
 
-  const Messages(
-      {super.key,
-      required this.date,
-      required this.isUser,
-      required this.message});
+  const Messages({
+    Key? key,
+    required this.date,
+    required this.isUser,
+    required this.message, required this.isFetching,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       padding: const EdgeInsets.all(15),
-      margin: const EdgeInsets.symmetric(vertical: 15).copyWith(
-        left: isUser ? 100 : 10,
-        right: isUser ? 10 : 100,
+      margin: EdgeInsets.symmetric(
+        vertical: 15,
+        horizontal: isUser ? 10 : 100,
       ),
       decoration: BoxDecoration(
-          color: isUser
-              ? const Color.fromARGB(255, 9, 48, 79)
-              : Colors.grey.shade300,
-          borderRadius: BorderRadius.only(
-              topLeft: const Radius.circular(10),
-              bottomLeft: isUser ? const Radius.circular(10) : Radius.zero,
-              topRight: const Radius.circular(10),
-              bottomRight: isUser ? Radius.zero : const Radius.circular(10))),
+        color: isUser ? Color.fromARGB(255, 9, 48, 79) : Colors.grey.shade300,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(10),
+          bottomLeft: isUser ? Radius.circular(10) : Radius.zero,
+          topRight: Radius.circular(10),
+          bottomRight: isUser ? Radius.zero : Radius.circular(10),
+        ),
+      ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -183,10 +151,14 @@ class Messages extends StatelessWidget {
             message,
             style: TextStyle(color: isUser ? Colors.white : Colors.black),
           ),
-          Text(
+          isFetching
+           ? Center(child: CircularProgressIndicator())
+           : Text(
             date,
             style: TextStyle(
-                color: isUser ? Colors.white : Colors.black, fontSize: 10),
+              color: isUser ? Colors.white : Colors.black,
+              fontSize: 10,
+            ),
           ),
         ],
       ),
@@ -198,6 +170,7 @@ class Message {
   final bool isUser;
   final String message;
   final DateTime date;
+
   Message({
     required this.date,
     required this.isUser,
